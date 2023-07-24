28c4062d256165256a23259c930ea784db726a81
koteld <festbot@gmail.com>
2023-07-24 21:25:02 +0200

    add tests for dataFeed and dataFeedProxy

---
+++ packages/hardhat-chainlink/test/fixture-projects/hardhat-chainlink/hardhat.config.ts
@@ -0,0 +1,28 @@
+import "@nomiclabs/hardhat-ethers";
+import { HardhatUserConfig } from "hardhat/types";
+
+import "../../../src/index";
+
+const commonCompilerSettings = {
+  optimizer: {
+    enabled: true,
+    runs: 200,
+  },
+};
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: commonCompilerSettings,
+      },
+      {
+        version: "0.7.6",
+        settings: commonCompilerSettings,
+      },
+    ],
+  },
+};
+
+export default config;
