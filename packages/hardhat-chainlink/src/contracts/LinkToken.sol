db69e54c7497fee319e147469b66214b1b9541cd
koteld <festbot@gmail.com>
2023-07-30 13:02:56 +0200

    remove example contracts

--- packages/hardhat-chainlink/src/contracts/LinkToken.sol
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.4.11;
-
-import "@chainlink/contracts/src/v0.4/LinkToken.sol";
--- packages/hardhat-chainlink/src/contracts/Oracle.sol
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.4.24;
-
-import "@chainlink/contracts/src/v0.4/Oracle.sol";
