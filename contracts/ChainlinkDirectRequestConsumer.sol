68da5bdde453b6bdfa9397e9ba7b92ef3278dbcc
koteld <festbot@gmail.com>
2023-08-25 13:06:45 +0200

    add sandbox contracts to root

---
+++ contracts/ChainlinkDirectRequestConsumer.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+import "@chainlink/contracts/src/v0.8/Chainlink.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
+ * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
+ * DO NOT USE THIS CODE IN PRODUCTION.
+ */
+
+contract ChainlinkDirectRequestConsumer is ChainlinkClient, ConfirmedOwner {
+  using Chainlink for Chainlink.Request;
+
+  uint256 private constant ORACLE_PAYMENT = 1 * LINK_DIVISIBILITY; // 1 * 10**18
+  uint256 public answer;
+
+  event RequestDataFulfilled(bytes32 indexed requestId, uint256 indexed answer);
+
+  /**
+   * @dev Check https://docs.chain.link/docs/link-token-contracts/ for LINK address for the right network
+     */
+  constructor(address linkTokenAddress) ConfirmedOwner(msg.sender) {
+    setChainlinkToken(linkTokenAddress);
+  }
+
+  function requestData(
+    address _oracle,
+    string memory _jobId,
+    string memory _url,
+    string memory _path,
+    int256 _times
+  ) public onlyOwner {
+    Chainlink.Request memory req = buildChainlinkRequest(
+      stringToBytes32(_jobId),
+      address(this),
+      this.fulfillData.selector
+    );
+    req.add('get', _url);
+    req.add('path', _path);
+    req.addInt('times', _times);
+    sendChainlinkRequestTo(_oracle, req, ORACLE_PAYMENT);
+  }
+
+  function fulfillData(bytes32 _requestId, uint256 _answer) public recordChainlinkFulfillment(_requestId) {
+    emit RequestDataFulfilled(_requestId, _answer);
+    answer = _answer;
+  }
+
+  function getChainlinkToken() public view returns (address) {
+    return chainlinkTokenAddress();
+  }
+
+  function withdrawLink() public onlyOwner {
+    LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+    require(link.transfer(msg.sender, link.balanceOf(address(this))), 'Unable to transfer');
+  }
+
+  function cancelRequest(
+    bytes32 _requestId,
+    uint256 _payment,
+    bytes4 _callbackFunctionId,
+    uint256 _expiration
+  ) public onlyOwner {
+    cancelChainlinkRequest(_requestId, _payment, _callbackFunctionId, _expiration);
+  }
+
+  function stringToBytes32(string memory source) private pure returns (bytes32 result) {
+    bytes memory tempEmptyStringTest = bytes(source);
+    if (tempEmptyStringTest.length == 0) {
+      return 0x0;
+    }
+
+    assembly {
+    // solhint-disable-line no-inline-assembly
+      result := mload(add(source, 32))
+    }
+  }
+}
